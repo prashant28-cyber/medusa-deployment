0cfc6655420be746cfd36028d550eaf880fee901
prashant28-cyber <prashantchavan454545@gmail.com>
2025-04-06 22:04:51 +0530

    Update variable.tf

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -2,3 +2,13 @@ variable "aws_region" {
   description = "AWS region"
   default     = "us-east-1"
 }
+
+variable "database_url" {
+  description = "PostgreSQL connection string"
+  type        = string
+}
+
+variable "redis_url" {
+  description = "Redis connection string"
+  type        = string
+}
